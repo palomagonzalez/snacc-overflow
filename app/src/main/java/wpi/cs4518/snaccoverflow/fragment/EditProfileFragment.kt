6d1e6a86d49cafba327e711624a8c3d46a8b880f
Mark Forte <markjforte2000@gmail.com>
2021-10-08 21:33:03 -0400

    Transitions between profile view and two sub views

---
+++ app/src/main/java/wpi/cs4518/snaccoverflow/fragment/EditProfileFragment.kt
@@ -0,0 +1,34 @@
+package wpi.cs4518.snaccoverflow.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import wpi.cs4518.snaccoverflow.R
+
+private const val TAG = "wpi.EditProfile"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [EditProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class EditProfileFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+    }
+
+}
--- app/src/main/java/wpi/cs4518/snaccoverflow/fragment/ProfileFragment.kt
+++ app/src/main/java/wpi/cs4518/snaccoverflow/fragment/ProfileFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.fragment.app.commit
 import wpi.cs4518.snaccoverflow.R
 
 private const val TAG = "wpi.ProfileFragment"
@@ -26,7 +28,27 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         Log.d(TAG, "Inflated")
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        // Edit Profile Button
+        view.findViewById<ImageButton>(R.id.buttonEditProfile)?.setOnClickListener {
+            val fragment = EditProfileFragment()
+            transitionToFragment(fragment)
+        }
+        // Edit Preferences Button
+        view.findViewById<ImageButton>(R.id.buttonEditPreferences)?.setOnClickListener {
+            val fragment = EditPreferencesFragment()
+            transitionToFragment(fragment)
+        }
+        return view
+    }
+
+    private fun transitionToFragment(fragment: Fragment) {
+        val fragmentManger = activity?.supportFragmentManager
+        fragmentManger?.commit {
+            replace(R.id.fragment_container, fragment)
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
     }
 
 }
