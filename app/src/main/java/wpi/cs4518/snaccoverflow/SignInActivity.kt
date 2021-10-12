ef2a0c491a62768acc72d2df84fd1cc1863fffa4
carleygilmore3@gmail.com <40573151+cgilmore18@users.noreply.github.com>
2021-10-11 21:15:51 -0400

    First part of chat user authentication complete- need syncing with profile page

---
+++ app/src/main/java/wpi/cs4518/snaccoverflow/SignInActivity.kt
@@ -0,0 +1,66 @@
+package wpi.cs4518.snaccoverflow
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import org.jetbrains.anko.clearTask
+import org.jetbrains.anko.design.longSnackbar
+import org.jetbrains.anko.indeterminateProgressDialog
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.newTask
+
+
+class SignInActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 1
+
+    private val signInProviders =
+        listOf(
+            AuthUI.IdpConfig.EmailBuilder()
+                .setAllowNewAccounts(true)
+                .setRequireName(true)
+                .build()
+        )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        account_sign_in.setOnClickListener {
+            val intent = AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(signInProviders)
+                .setLogo(R.drawable.ic_fire_emoji)
+                .build()
+            startActivityForResult(intent, RC_SIGN_IN)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val progressDialog = indeterminateProgressDialog("Setting up your account")
+                //TODO init current user in Firestore
+                startActivity(intentFor<MainActivity>().newTask().clearTask())
+                progressDialog.dismiss()
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                if (response == null) return
+
+                when (response.error?.errorCode) {
+                    ErrorCodes.NO_NETWORK ->
+                        constraint_layout.longSnackbar("No network")
+                    ErrorCodes.UNKNOWN_ERROR ->
+                        constraint_layout.longSnackbar("Unknown error")
+                }
+            }
+        }
+    }
+}
